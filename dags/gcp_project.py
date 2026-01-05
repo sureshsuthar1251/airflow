72fa3ff0d390a209e59827efa6e51bf592582340
Suresh Suthar <85287250+sureshsuthar1251@users.noreply.github.com>
2026-01-05 14:39:15 +0530

    Update gcp_project.py

--- dags/gcp_project.py
+++ dags/gcp_project.py
@@ -1,21 +1,24 @@
 from airflow import DAG
-from datetime import datetime
 from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from datetime import datetime
 
-def files_inside_gcs_bucket():
-  hook = GCSHook()
-  files = hook.list(
-        bucket_name="bkt_test_suresh_1",
+def list_gcs_objects():
+    hook = GCSHook()
+    files = hook.list(
+        bucket_name="my-bucket",
         prefix="input/"
     )
-  print(files)
-  
+    print(files)
 
-
-dag = DAG(dag_id = 'gcp_dummy_project', schedule = None,start_date = datetime(2026,01,05)   )
-list_gcs_objects = PythonOperator(
-  task_id = 'list_gcs_objects',
-  python_callable = 'files_inside_gcs_bucket'
+dag =  DAG(
+    dag_id="composer_list_gcs_objects",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False
 )
-  
+
+list_files = PythonOperator(
+    task_id="list_files",
+    python_callable=list_gcs_objects
+)
